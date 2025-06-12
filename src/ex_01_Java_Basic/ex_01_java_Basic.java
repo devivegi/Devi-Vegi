3475b4f085ab9c3d53c51e31b9efa115c61c5e1d
devivegi <devi22vegi@gmail.com>
2025-06-12 21:37:30 +0530

    Testing

---
+++ src/ex_01_Java_Basic/ex_01_java_Basic.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class ex_01_java_Basic {
+    public static void main(String [] args){
+        System.out.println("Hello     Java");
+        System.out.println("Hello     Java");
+
+
+    }
+}
+
---
+++ src/ex_01_Java_Basic/tester.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class tester {
+    public static void main(String[] args) {
+
+    }
+}
