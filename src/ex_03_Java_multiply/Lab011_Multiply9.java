9ce257b325c9304f1817bf25389e869e879e34a4
devivegi <devi22vegi@gmail.com>
2025-06-15 08:54:34 +0530

    Testing

---
+++ src/ex_03_Java_multiply/Lab011_Multiply9.java
@@ -0,0 +1,22 @@
+package ex_03_Java_multiply;
+
+public class Lab011_Multiply9 {
+    public static void main(String[] args) {
+        int number =9;
+
+        System.out.println("Mutiplication of table" +number);
+        System.out.println(number + "*1=" + (number*1));
+        System.out.println(number + "*2=" + (number*2));
+        System.out.println(number + "*3=" + (number*3));
+        System.out.println(number + "*4=" + (number*4));
+        System.out.println(number + "*5=" + (number*5));
+        System.out.println(number + "*6=" + (number*6));
+        System.out.println(number + "*7=" + (number*7));
+        System.out.println(number + "*8=" + (number*8));
+        System.out.println(number + "*9=" + (number*9));
+        System.out.println(number + "*10=" + (number*10));
+
+
+
+    }
+}
